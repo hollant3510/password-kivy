254d640623cabd2740202b68056f242bbf3fc1fb
EthanZeigler <ethanzeigler@gmail.com>
2017-09-24 22:45:14 -0400

    First version of layout

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from kivy.app import App
+from kivymd.theming import ThemeManager
+from kivymd.button import MDRaisedButton
+from kivymd.button import *
 
 
 class PasswordApp(App):
+    theme_cls = ThemeManager()
+    menu_items = [{'viewclass': 'MDMenuItem', 'text': 'MD5', "on_press": "app.show_example_date_picker(1,1)"}]
+
     pass
 
 if __name__ == '__main__':
