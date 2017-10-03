e437bb651eb7907d67a0eab719c8d6cd081b41c1
EthanZeigler <ethanzeigler@gmail.com>
2017-10-03 17:03:38 -0400

    add new arrow, change text color (which was really annoying due to poor documentation)

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from kivymd.button import *
 
 class PasswordApp(App):
     theme_cls = ThemeManager()
-    menu_items = [{'viewclass': 'MDMenuItem', 'text': 'MD5', "on_press": "app.show_example_date_picker(1,1)"}]
+
+    menu_items = [{'viewclass': 'MDMenuItem', 'text': 'MD5', "on_press": "app.show_example_date_picker(1,1)"},
+                  {'viewclass': 'MDMenuItem', 'text': 'SHA-1', "on_press": "app.show_example_date_picker(1,1)"},
+                  {'viewclass': 'MDMenuItem', 'text': 'SHA-256', "on_press": "app.show_example_date_picker(1,1)"}]
 
     pass
 
