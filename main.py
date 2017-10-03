5aea8dc88c5574db6f4959cedbd1e9e336d8495e
hollant3510 <hollant4@tcnj.edu>
2017-10-03 18:40:51 -0400

    Started adding of features but need generate button

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from kivy.app import App
+import kivy
 
+from kivy.app import App
+from kivymd.theming import ThemeManager
+from kivymd.button import MDRaisedButton
+from kivymd.button import *
+from kivy.uix.boxlayout import BoxLayout
+
+
+class CreatePasswordForm(BoxLayout):
+     
+    def encodepass(self, o, m1, m2):
+        return (o + m1 + m2)
+        
+ 
 
 class PasswordApp(App):
+    theme_cls = ThemeManager()
+    menu_items = [{'viewclass': 'MDMenuItem', 'text': 'MD5', "on_press": "app.show_example_date_picker(1,1)"}]
+
     pass
 
 if __name__ == '__main__':
